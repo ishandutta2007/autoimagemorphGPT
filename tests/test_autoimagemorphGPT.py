634604cf5e7d33b35f63ad2cedfab1b15e5f2752
basuda <66863247+basuda@users.noreply.github.com>
2023-05-07 23:59:32 +0530

    minor

--- tests/test_autoimagemorphGPT.py
+++ tests/test_autoimagemorphGPT.py
@@ -1,5 +1,5 @@
 from autoimagemorphGPT import run_autoimagemorphGPT
 
 def test_run_autoimagemorph():
-    # python autoimagemorphGPT.py -inframes "['f0.png','f30.png','f60.png','f90.png','f120.png','f150.png','f0.png']" -outprefix f -framerate 30 -subpixel 4
+    # python autoimagemorphGPT/autoimagemorphGPT.py -inframes "['f0.png','f30.png','f60.png','f90.png','f120.png','f150.png','f0.png']" -outprefix f -framerate 30 -subpixel 4
     run_autoimagemorphGPT()#inframes="['f0.png','f30.png','f60.png','f90.png','f120.png','f150.png','f0.png']", outprefix="f", framerate=30, subpixel=4)
