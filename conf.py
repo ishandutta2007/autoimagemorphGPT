d1dbaa45b81532805ab20282206221a08c49c6b2
ishan dutta <ishandutta2007@gmail.com>
2023-05-10 06:54:18 +0530

    disable the sphinx.builders.linkcheck'

--- conf.py
+++ conf.py
@@ -26,4 +26,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.builders.linkcheck']
